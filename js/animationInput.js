054a4fd2d429848f8f97948627e69e33ed5de2d8
Daniel San <danielgdt88@hotmail.com>
2024-06-04 18:08:08 -0300

    att de blocos

--- js/animationInput.js
+++ js/animationInput.js
@@ -34,6 +34,6 @@ export default function animacaoFormOnChange() {
 
         verificaEstado();
       });
-    }, 700);
+    }, 300);
   });
 }
--- js/buttonEnabled.js
+++ js/buttonEnabled.js
@@ -1,6 +1,6 @@
 export default function buttonEnabled() {
   const inputElement = document.querySelector("[data-input]");
-  const labelElement = document.querySelector("[data-label]");
+  const containerElement = document.querySelector("[data-container]");
   const divMainElement = document.querySelector("[data-divmain]");
   const imgElement = document.querySelector("[data-image]");
   addEventListener("DOMContentLoaded", () => {
@@ -18,5 +18,7 @@ export default function buttonEnabled() {
   function paginaValidation() {
     divMainElement.style.display = "none";
     imgElement.style.display = "none";
+    containerElement.style.height = "62%";
+    containerElement.style.width = "30%";
   }
 }
