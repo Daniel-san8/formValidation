c700e4485e2b637613d6c5b54029daaa508c618d
Daniel San <danielgdt88@hotmail.com>
2024-06-06 23:38:29 -0300

    novas functions

--- js/buttonEnabled.js
+++ js/buttonEnabled.js
@@ -3,6 +3,9 @@ export default function buttonEnabled() {
   const bodyElement = document.querySelector("body");
   const divMainElement = document.querySelector("[data-divmain]");
   const imgElement = document.querySelector("[data-image]");
+  const inputValue = document.querySelector("[data-input]");
+  const dataStrongVariavel = document.querySelector("[data-strongVariavel]");
+
   addEventListener("DOMContentLoaded", () => {
     const buttonAncora = document.querySelector("[data-ancora]");
     buttonAncora.classList.add("desativado");
@@ -18,38 +21,50 @@ export default function buttonEnabled() {
 
   function paginaValidation() {
     const novaPagina = `<div class="container-new-page">
-    <div class="div-page-confirm">
+      <div class="div-page-confirm">
         <img class="image-new-page" src="/appProject/images/icon-success.svg" alt="success">
         <h1 class="title-new-page">Thanks for subscribing!</h1>
         <p class="para-new-page">A confirmation email has been sent to <strong
-                data-strongVariavel>teste@hotmail.com</strong>. Please open
+                data-strongVariavel>${inputValue.value}</strong>. Please open
             it and click <br> the
             button inside to confirm your subscription.</p>
         <a href="" class="ancora-button-new-page">Subscribe to monthly newsletter</a>
-    </div>`;
+      </div>`;
 
     divMainElement.style.display = "none";
     imgElement.style.display = "none";
     containerElement.style.display = "none";
     containerElement.style.width = "30%";
     bodyElement.insertAdjacentHTML("beforeend", novaPagina);
+
     const containerElementNew = document.querySelector(".container-new-page");
     const divPage = document.querySelector(".div-page-confirm");
     divPage.style.gap = "10px";
     containerElementNew.style.display = "flex";
     containerElementNew.style.gap = "100px";
+
     const buttonNewPage = document.querySelector(".ancora-button-new-page");
     buttonNewPage.addEventListener("click", (e) => {
       e.preventDefault();
+      dismissMessage();
     });
 
-    buttonNewPage.addEventListener("click", dismissMessage);
-
     buttonNewPage.innerHTML = "Dismiss message";
-    console.log(buttonNewPage);
   }
 
   function dismissMessage() {
-    console.log("oi");
+    const containerElementNew = document.querySelector(".container-new-page");
+
+    // Remove o elemento do DOM
+    if (containerElementNew) {
+      containerElementNew.parentNode.removeChild(containerElementNew);
+    }
+
+    // Restaura a visibilidade e o estilo dos elementos originais
+    divMainElement.style.display = "flex";
+    imgElement.style.display = "flex";
+    containerElement.style.display = "flex";
+    containerElement.style.width = "60%";
+    containerElement.style.height = "90%";
   }
 }
